650d9c84093037ae28dabbea88be7a2a10e33342
Matias Sendra <mjs.sendra@gmail.com>
2023-08-21 11:43:24 -0300

    Fin de desarrollo de aplicacion

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'shared-lazy-image',
+    templateUrl: './lazy-image.component.html',
+
+})
+export class LazyImageComponent implements OnInit {
+
+    @Input()
+    public url!: string;
+
+
+    @Input()
+    public alt: string = '';
+
+
+    public hashLoaded: boolean = false;
+
+
+    ngOnInit(): void {
+        if (!this.url) throw new Error('URL property is required');
+    }
+
+    onLoad(){
+
+        setTimeout(() => {
+            this.hashLoaded = true;
+        }, 1000);
+
+    }
+
+
+}
